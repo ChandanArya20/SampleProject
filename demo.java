0e458333b335e2d7463d71753209288bd674d4b7
chandan <chandank1848@gmail.com>
2022-10-19 12:09:54 +0530

    First commit

---
+++ demo.java
@@ -0,0 +1,20 @@
+public class dataTypes {
+    public static void main(String []arg)
+    {
+        // byte a=32;
+        // byte b=120;
+        // short c = (short)(a+b);
+        // System.out.println(c);
+        // System.out.println("");
+        char c;
+        String s1="Chandan";
+        String s2="anup";
+        String s3=s1+s2;
+        int i=s1.compareToIgnoreCase(s2);
+        System.out.println(i);
+        // for(c=2309;c<2362;c++)
+        // {
+        //     System.out.println(c);
+        // }
+    }
+}
